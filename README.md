6cd6fd90501dce1987e756783e4f84e839268661
Zhang Kaiyu <yeannylam@gmail.com>
2019-05-26 10:20:44 +0800

    add implementation

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Smart Beginning of Line
+
+Jump to the first non-whitespace character by first tap. If at first
+non-whitespace character, jump to line start.
--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+const { Position, Selection } = vscode;
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
@@ -6,7 +7,27 @@ export function activate(context: vscode.ExtensionContext) {
     () => {
       const editor = vscode.window.activeTextEditor;
       if (!editor) return;
-      vscode.commands.executeCommand('cursorLineStart');
+      let allCursorsAtSBA = true;
+      for (const sel of editor.selections) {
+        const line = editor.document.lineAt(sel.active);
+        if (sel.active.character !== line.firstNonWhitespaceCharacterIndex) {
+          allCursorsAtSBA = false;
+          break;
+        }
+      }
+      editor.selections = editor.selections.map((sel) => {
+        if (allCursorsAtSBA) {
+          const position = new Position(sel.active.line, 0);
+          return new Selection(position, position);
+        } else {
+          const line = editor.document.lineAt(sel.active);
+          const position = new Position(
+            sel.active.line,
+            line.firstNonWhitespaceCharacterIndex
+          );
+          return new Selection(position, position);
+        }
+      });
     }
   );
   context.subscriptions.push(disposable);
